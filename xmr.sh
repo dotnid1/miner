ed64f30057ca2b30c81672f0626a5b06f479a109
dotnid1 <87577885+dotnid1@users.noreply.github.com>
2021-07-18 00:06:32 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -2,4 +2,4 @@
 wget https://github.com/xmrig/xmrig/releases/download/v5.5.1/xmrig-5.5.1-xenial-x64.tar.gz
 tar -zxf xmrig-5.5.1-xenial-x64.tar.gz
 cd xmrig-5.5.1
-./xmrig -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u dodotnanda@gmail.com -p x </dev/null &>/dev/null &
+./xmrig -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u dodotnanda@gmail.com -p x
